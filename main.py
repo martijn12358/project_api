0d8e93bdb601557d28dc8c0c5865242a324312be
martijn gerritsen <martijn12358@gmail.com>
2023-03-23 22:07:30 +0100

    updated

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from flask import Flask, jsonify, request
 
+
 app = Flask(__name__)
 
 @app.route('/project-data', methods = ['GET'])
 def projectdata():
-    date = request.args.get('date')
-    if date is None:
+    startdate = request.args.get('startdate')
+    enddate = request.args.get('enddate')
+    if startdate is None:
         text = "invalid date"
     else:
-        text = date
-    return jsonify({'message': text})
+        text = startdate
+    return jsonify({'data': [{'date': startdate, 'KM/H': '13'}, {'date': startdate, 'KM/H': '12'}]})
 
 if __name__ == '__main__':
     app.run(debug=True)
