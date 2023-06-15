afab6327d306e32fa549201860a0e1160e32c8c5
Martijn Gerritsen <martijn12358@gmail.com>
2023-06-15 21:34:16 +0200

    mqtt test

--- mqtt_broker.py
+++ mqtt_broker.py
@@ -21,6 +21,7 @@ def on_message(client, userdata, msg):
 def run():
 
     client = mqtt.Client()
+    print("test")
     client.on_connect = on_connect
     client.on_message = on_message
     client.username_pw_set(username, password)
