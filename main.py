694ced9632c76560874d0338f347ee76f8a5a007
Martijn Gerritsen <martijn12358@gmail.com>
2023-06-25 10:24:00 +0200

    updated main class

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-import api
-import database_functions
-import mqtt_broker
+from api import Api
 import threading
+from database_functions import Database
+from mqtt_broker import mqtt_broker
+
+broker = "eu1.cloud.thethings.network"
+port = 1883
+username = "application-test-bike-mileage@ttn"
+password = "NNSXS.KGQ3626QBCSRLOFIZSWGV6VH5B3LOOYFFCESSDI.GB5XQX3T45IWOABT3ZOYMGAJ7TRJ554MH7TPE6Q6ALBKHSC6WNJA"
+db_host = "localhost"
+db_user = "project_dev"
+db_password = "project_password"
+db = "data"
 
 
 try:
-    database_functions.connect_database()
-    database_functions.database_checker()
-    database_functions.reset_database()
-    database_functions.fill_database()
+    database = Database(host=db_host, user=db_user, password=db_password, database=db)
 
-    thread_api = threading.Thread(target=api.app.run)
-    thread_mqtt = threading.Thread(target=mqtt_broker.run)
+    database.connect_database()
+    database.database_checker()
+    database.reset_database()
+    database.fill_database()
+
+    mqtt_client = mqtt_broker(broker=broker, port=port, username=username, password=password, database=database)
+    api = Api(database=database)
+
+    thread_api = threading.Thread(target=api.run)
+    thread_mqtt = threading.Thread(target=mqtt_client.run)
     try:
         thread_api.start()
     except Exception as e:
